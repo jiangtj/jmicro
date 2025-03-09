0ec2aa9a2b1bb9cbeb10e02cf55eda564b217efe
Mr.J <116749895@qq.com>
2025-03-09 12:14:55 +0800

    refactor: 重构项目并移除不必要的代码
    - 删除了多个 GitHub Actions 工作流文件- 移除了 Spring Cloud 相关的依赖和注解
    - 更新了 Maven 配置文件- 删除了未使用的 Java 文件和包

--- demo-backend/src/main/java/com/jiangtj/platform/baseservlet/ServletProblemDetailsExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.jiangtj.platform.baseservlet;
-
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-//@Component
-//@ControllerAdvice
-public class ServletProblemDetailsExceptionHandler extends ResponseEntityExceptionHandler {
-}
