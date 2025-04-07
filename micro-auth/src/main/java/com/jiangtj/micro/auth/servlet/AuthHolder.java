5edfe7078e10e54485e507dd08d1892018d818ed
Mr.J <116749895@qq.com>
2025-04-08 00:26:35 +0800

    refactor(auth): 重构 AuthHolder 类
    - 移除了对 HttpServletRequest 的直接依赖
    - 使用 ServletRequestAttributes 替代 RequestAttributes
    - 新增 currentRequestAttributes() 方法获取当前 ServletRequestAttributes 实例
    - 优化了 getAuthContext() 和 setAuthContext() 方法的实现

--- micro-auth/src/main/java/com/jiangtj/micro/auth/servlet/AuthHolder.java
+++ micro-auth/src/main/java/com/jiangtj/micro/auth/servlet/AuthHolder.java
@@ -4,35 +4,43 @@ import com.jiangtj.micro.auth.AuthRequestAttributes;
 import com.jiangtj.micro.auth.context.AuthContext;
 import com.jiangtj.micro.auth.context.AuthContextFactory;
 import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
-
-import java.util.Objects;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class AuthHolder {
 
     @Resource
-    private HttpServletRequest request;
-    @Resource
     private AuthContextFactory factory;
 
     public AuthContext getAuthContext() {
-        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        Objects.requireNonNull(requestAttributes);
+        ServletRequestAttributes requestAttributes = currentRequestAttributes();
         AuthContext context = (AuthContext) requestAttributes.getAttribute(AuthRequestAttributes.AUTH_CONTEXT_ATTRIBUTE, RequestAttributes.SCOPE_REQUEST);
         if (context == null) {
-            context = factory.getAuthContext(new ServletServerHttpRequest(request));
-            setAuthContext(context);
+            context = factory.getAuthContext(new ServletServerHttpRequest(requestAttributes.getRequest()));
+            requestAttributes.setAttribute(AuthRequestAttributes.AUTH_CONTEXT_ATTRIBUTE, context, RequestAttributes.SCOPE_REQUEST);
         }
         return context;
     }
 
     public void setAuthContext(AuthContext ctx) {
-        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        Objects.requireNonNull(requestAttributes);
+        ServletRequestAttributes requestAttributes = currentRequestAttributes();
         requestAttributes.setAttribute(AuthRequestAttributes.AUTH_CONTEXT_ATTRIBUTE, ctx, RequestAttributes.SCOPE_REQUEST);
+    }
+
+    /**
+     * Return the current RequestAttributes instance as ServletRequestAttributes.
+     * @see RequestContextHolder#currentRequestAttributes()
+     *
+     * copy from WebApplicationContextUtils
+     */
+    private static ServletRequestAttributes currentRequestAttributes() {
+        RequestAttributes requestAttr = RequestContextHolder.currentRequestAttributes();
+        if (!(requestAttr instanceof ServletRequestAttributes servletRequestAttributes)) {
+            throw new IllegalStateException("Current request is not a servlet request");
+        }
+        return servletRequestAttributes;
     }
 
 }
