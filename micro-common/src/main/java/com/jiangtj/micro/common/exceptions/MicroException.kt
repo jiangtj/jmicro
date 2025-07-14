528ea050a2e836d43060dbaebe56be0975f45c7b
Mr.J <116749895@qq.com>
2025-07-14 15:44:41 +0800

    Add documentation to MicroException, MicroHttpException, and MicroProblemDetailException

--- micro-common/src/main/java/com/jiangtj/micro/common/exceptions/MicroException.kt
+++ micro-common/src/main/java/com/jiangtj/micro/common/exceptions/MicroException.kt
@@ -1,5 +1,8 @@
 package com.jiangtj.micro.common.exceptions
 
+/**
+ * 自定义异常类，继承自 [RuntimeException]，作为项目中自定义异常的基类。
+ */
 open class MicroException : RuntimeException {
     constructor() : super()
 
--- micro-common/src/main/java/com/jiangtj/micro/common/exceptions/MicroProblemDetailException.kt
+++ micro-common/src/main/java/com/jiangtj/micro/common/exceptions/MicroProblemDetailException.kt
@@ -1,5 +1,8 @@
 package com.jiangtj.micro.common.exceptions
 
+/**
+ * 自定义异常类，继承自 [MicroHttpException]，用于表示包含问题详情的 HTTP 异常。
+ */
 open class MicroProblemDetailException : MicroHttpException {
     val title: String
     val detail: String
